81460e3c6ab9ef4b384e0f94c4e43ffb039def03
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-20 19:34:53 +0300

    Ejs added.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const ejs = require('ejs')
 
 const app = express();
 
-const blog = {
-    id: 1,
-    title: 'Blog title',
-    desc: 'Blog desc'
-}
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send(blog)
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
 })
 
 const port = 3000;
